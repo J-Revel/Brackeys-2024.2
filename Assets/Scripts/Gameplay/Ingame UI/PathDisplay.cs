480b1f2415cfbb9b4ee7988a7877524569cdff90
J-Revel <j.revel@protonmail.com>
2024-09-13 18:08:15 +0200

    Star path wip

--- Assets/Scripts/Gameplay/Ingame UI/PathDisplay.cs
+++ Assets/Scripts/Gameplay/Ingame UI/PathDisplay.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 public class PathDisplay : MonoBehaviour
 {
@@ -12,7 +14,15 @@ public class PathDisplay : MonoBehaviour
     public float3[] previous_path;
     public float path_transition_duration = 0.3f;
     private float path_transition_time;
+    private uint random_seed;
+    public float2 random_offset_scale = new float2(0.2f, 0.5f);
+    public GameObject[] point_prefabs;
+    public List<GameObject> spawned_points = new List<GameObject>();
 
+    private void Start()
+    {
+        random_seed = (uint)DateTime.Now.Ticks;
+    }
     public void SetPath(int2 start_cell, int2[] new_path)
     {
         if (math.any(start_cell != this.start_cell))
@@ -36,6 +46,10 @@ public class PathDisplay : MonoBehaviour
 
             if (path_changed)
             {
+                foreach(GameObject to_remove in spawned_points)
+                    Destroy(to_remove);
+                spawned_points.Clear();
+                random_seed = (uint)DateTime.Now.Ticks;
                 previous_path = new float3[math.max(new_path.Length, previous_path.Length)];
                 for (int i = 0; i < previous_path.Length; i++)
                 {
@@ -44,7 +58,7 @@ public class PathDisplay : MonoBehaviour
                     else previous_path[i] = line_renderer.GetPosition(line_renderer.positionCount - 1);
                 }
 
-                path = new int2[previous_path.Length];
+                path = new int2[new_path.Length];
                 actual_path_length = new_path.Length;
                 for (int i = 0; i < path.Length; i++)
                 {
@@ -54,6 +68,8 @@ public class PathDisplay : MonoBehaviour
                 }
 
                 path_transition_time = 0;
+                for (int i = 0; i < path.Length; i++)
+                    spawned_points.Add(Instantiate(point_prefabs[UnityEngine.Random.Range(0, point_prefabs.Length)]));
             }
         }
     }
@@ -62,13 +78,17 @@ public class PathDisplay : MonoBehaviour
     {
         path_transition_time += Time.deltaTime;
         line_renderer.positionCount = path.Length + 1;
+        Random random = new Random(random_seed);
         line_renderer.SetPosition(0, GridInstance.instance.CellToPos(start_cell));
         for (int i = 0; i < path.Length; i++)
         {
+            float2 random_offset = random.NextFloat2();
             float3 target_pos = GridInstance.instance.CellToPos(path[i]);
-            float3 start_pos = previous_path[i];
-            float f = math.saturate(path_transition_time / path_transition_duration);
-            line_renderer.SetPosition(i+1, math.lerp(start_pos, target_pos, f));
+            float offset = math.lerp(random_offset_scale.x, random_offset_scale.y, random_offset.y);
+            target_pos.x += offset * math.cos(random_offset.x * math.PI * 2);
+            target_pos.y += offset * math.sin(random_offset.x * math.PI * 2);
+            line_renderer.SetPosition(i+1, target_pos);
+            spawned_points[i].transform.position = target_pos;
         }
     }
 }

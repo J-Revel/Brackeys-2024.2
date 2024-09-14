63bf36b4ef6413b9dcc39832247b9a36a6eedca6
J-Revel <j.revel@protonmail.com>
2024-09-14 16:42:49 +0200

    Added sound to tiles + fix sound in event menu

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using FMOD.Studio;
 using FMODUnity;
+using UI;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public EventReference music_event;
     public EventReference ambience_event;
+    private float music_eq_time;
+    public float music_eq_transition_duration = 0.5f;
 
     public void Start()
     {
@@ -15,5 +18,14 @@ public class AudioManager : MonoBehaviour
         music_instance.start();
         EventInstance ambience_instance = FMODUnity.RuntimeManager.CreateInstance(ambience_event);
         ambience_instance.start();
+        music_eq_time = music_eq_transition_duration;
+    }
+
+    public void Update()
+    {
+        if (MenuSystem.instance.active_menu != null)
+        {
+            
+        }
     }
 }
--- Assets/Scripts/Gameplay/ShelterCell.cs
+++ Assets/Scripts/Gameplay/ShelterCell.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,16 +11,17 @@ public class ShelterCell : MonoBehaviour
     private CellEntity cell_entity;
     public ResourceStock[] cost;
     public Sprite action_icon;
+    public EventReference action_sound;
     
     public void Start()
     {
         cell_entity = GetComponent<CellEntity>();
         CellContent cell_content = GridInstance.instance.GetCellContent(cell_entity.cell);
-        cell_content.enter_coroutines.Add(ActionPopupMenu.instance.ShowActionCoroutine(action_icon, cost));
+        cell_content.enter_coroutines.Add(ActionPopupMenu.instance.ShowActionCoroutine(action_icon, action_sound, cost));
         cell_content.enter_coroutines_finished += () =>
         {
-            cell_content.enter_coroutines.Add(ActionPopupMenu.instance.ShowActionCoroutine(action_icon, cost));
+            cell_content.enter_coroutines.Add(ActionPopupMenu.instance.ShowActionCoroutine(action_icon, action_sound, cost));
         };
-
+        
     }
 }

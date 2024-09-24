e2c79367106cb8c5bf6f5100a363d9412f09e081
J-Revel <j.revel@protonmail.com>
2024-09-24 17:26:46 +0200

    Fog rendering optimized

--- Assets/Scripts/Gameplay/FogCell.cs
+++ Assets/Scripts/Gameplay/FogCell.cs
@@ -8,21 +8,25 @@ public class FogCell : MonoBehaviour
 {
     private float time = 0;
     public ParticleSystem[] particle_systems;
+    public SpriteRenderer[] sprite_renderers;
     public float alpha = 0;
 
     void Start()
     {
+        Color color = new Color(1, 1, 1, alpha * alpha * alpha);
         foreach (ParticleSystem particle_system in particle_systems)
-        {
             particle_system.customData.SetColor(ParticleSystemCustomData.Custom1, new Color(1, 1, 1, alpha * alpha * alpha));
-        }
+        foreach (SpriteRenderer sprite_renderer in sprite_renderers)
+            sprite_renderer.color = color;
         GetComponent<ParticleSystem>().Play();
     }
     void Update()
     {
+        Color color = new Color(1, 1, 1, alpha * alpha * alpha);
         foreach (ParticleSystem particle_system in particle_systems)
-        {
-            particle_system.customData.SetColor(ParticleSystemCustomData.Custom1, new Color(1, 1, 1, alpha * alpha * alpha));
-        }
+            particle_system.customData.SetColor(ParticleSystemCustomData.Custom1, color);
+
+        foreach (SpriteRenderer sprite_renderer in sprite_renderers)
+            sprite_renderer.color = color;
     }
 }
